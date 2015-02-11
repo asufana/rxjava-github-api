c1914f49be29292466c50fb89811698bcca92541
hana <asufanah@gmail.com>
2015-02-11 22:00:54 +0900

    Added dependencies

--- src/main/java/com/github/asufana/TestSample.java
+++ src/main/java/com/github/asufana/TestSample.java
@@ -4,11 +4,15 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 import org.junit.*;
+import org.slf4j.*;
 
 public class TestSample {
+    
+    private static final Logger logger = LoggerFactory.getLogger(TestSample.class);
     
     @Test
     public void test() throws Exception {
         assertThat(true, is(true));
+        logger.error("Log Test");
     }
 }
